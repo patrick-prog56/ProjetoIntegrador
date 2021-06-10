c8e510a2ca11492760e916b04b813f9b8242ea7f
Carolina <ribeiro.carolribeiro.carolina@gmail.com>
2021-06-10 11:43:05 -0300

    Adicionando a camada de Security e Service

---
+++ Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/repository/RepositoryUsuario.java
@@ -0,0 +1,12 @@
+package br.org.generation.projetoIntegrador.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.org.generation.projetoIntegrador.model.Usuario;
+
+
+public interface RepositoryUsuario extends JpaRepository<Usuario, Long>{
+    public Optional<Usuario> findByUsuario(String usuario);
+}
