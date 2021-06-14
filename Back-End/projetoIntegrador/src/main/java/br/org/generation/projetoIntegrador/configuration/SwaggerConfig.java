6cf59e1c4145e3e7cbeebb0679f4d99f4512ce01
helenamanjko <helenamanjko@gmail.com>
2021-06-14 15:53:21 -0300

    alteracao deploy e documentacao

--- Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/configuration/SwaggerConfig.java
+++ Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/configuration/SwaggerConfig.java
@@ -20,47 +20,30 @@ import java.util.List;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("br.org.generation.projetoIntegrador.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metadata())
-                .useDefaultResponseMessages(false)
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("br.org.generation.projetoIntegrador.controller"))
+                .paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false)
                 .globalResponses(HttpMethod.GET, responseMessageForGET());
     }
     public static ApiInfo metadata() {
-        return new ApiInfoBuilder()
-                .title("API - Projeto Integrador")
-                .description("Projeto API Spring - Girls Camp")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("http://localhost:8080/swagger-ui/")
-                .contact(contact())
-                .build();
+        return new ApiInfoBuilder().title("API - Projeto Integrador").description("Projeto API Spring - Girls Camp")
+                .version("1.0.0").license("Apache License Version 2.0").licenseUrl("http://localhost:8080/swagger-ui/")
+                .contact(contact()).build();
     }
     private static Contact contact() {
-        return new Contact("melhor grupo turma 23",
-                "https://github.com/gilsonmirandaj/ProjetoIntegrador",
-                "projetogirlscamp@gmail.com");
+        return new Contact("melhor grupo turma 23", "https://github.com/gilsonmirandaj/ProjetoIntegrador", "projetogirlscamp@gmail.com");
     }
+    
     private static List<Response> responseMessageForGET() {
         return new ArrayList<Response>() {
             private static final long serialVersionUID = 1L;
             {
-                add(new ResponseBuilder().code("200")
-                        .description("Sucesso!").build());
-                add(new ResponseBuilder().code("201")
-                        .description("Objeto Criado!").build());
-                add(new ResponseBuilder().code("401")
-                        .description("Não Autorizado!").build());
-                add(new ResponseBuilder().code("403")
-                        .description("Proibido!").build());
-                add(new ResponseBuilder().code("404")
-                        .description("Não Encontrado!").build());
-                add(new ResponseBuilder().code("500")
-                        .description("Erro!").build());
+                add(new ResponseBuilder().code("200").description("Sucesso!").build());
+                add(new ResponseBuilder().code("201").description("Objeto Criado!").build());
+                add(new ResponseBuilder().code("401").description("Não Autorizado!").build());
+                add(new ResponseBuilder().code("403").description("Proibido!").build());
+                add(new ResponseBuilder().code("404").description("Não Encontrado!").build());
+                add(new ResponseBuilder().code("500").description("Erro!").build());
             }
         };
     }
--- Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/security/BasicSecurityConfig.java
+++ Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/security/BasicSecurityConfig.java
@@ -10,8 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -31,7 +30,6 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		
 		return new BCryptPasswordEncoder();
 	}
 	
