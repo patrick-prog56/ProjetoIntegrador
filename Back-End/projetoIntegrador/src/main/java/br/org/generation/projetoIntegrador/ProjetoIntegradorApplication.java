2da6cf551e669a360e8e4e4234493a9f4c757569
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-06-14 12:08:45 -0300

    documentacao swager

--- Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/ProjetoIntegradorApplication.java
+++ Back-End/projetoIntegrador/src/main/java/br/org/generation/projetoIntegrador/ProjetoIntegradorApplication.java
@@ -2,9 +2,10 @@ package br.org.generation.projetoIntegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-public class ProjetoIntegradorApplication {
+public class ProjetoIntegradorApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoIntegradorApplication.class, args);
