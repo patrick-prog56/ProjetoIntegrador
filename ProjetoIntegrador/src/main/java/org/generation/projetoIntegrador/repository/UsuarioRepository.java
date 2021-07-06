d06269b585e43445e7fbb5ca84d7a735655e43fb
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-07-06 12:21:54 -0300

    alteração

--- ProjetoIntegrador/src/main/java/org/generation/projetoIntegrador/repository/UsuarioRepository.java
+++ ProjetoIntegrador/src/main/java/org/generation/projetoIntegrador/repository/UsuarioRepository.java
@@ -9,6 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	public Optional<Usuario> findByUsuario(String usuario);
 	public List<Usuario> findAllByUsuarioContainingIgnoreCase(String usuario);
 }
